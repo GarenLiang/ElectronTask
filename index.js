27bf689d8c0039cca0ef691a191ae3328d2b281e
GarenLiang <ideabreeze@gmail.com>
2017-05-23 16:28:56 -0700

    Visibility

--- index.js
+++ index.js
@@ -4,14 +4,24 @@ const electron = require('electron');
 const { app, BrowserWindow, Tray } = electron;
 
 let mainWindow;
-
+let tray;
 app.on('ready', () => {
   mainWindow = new BrowserWindow({
     titleBarStyle: 'hidden',
+    height: 550,
+    width: 350,
+    show: false
   });
   mainWindow.loadURL(`file://${__dirname}/src/index.html`);
 
   const iconName = process.platform === 'win32' ? 'windows-icon.png' : 'list.png';
   const iconPath = path.join(__dirname, `./src/assets/${iconName}`);
-  new Tray(iconPath);
+  tray = new Tray(iconPath);
+  tray.on('click', () => {
+    if (mainWindow.isVisible()) {
+      mainWindow.hide();
+    } else {
+      mainWindow.show();
+    }
+  });
 });
