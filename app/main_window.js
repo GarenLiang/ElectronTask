47c113193f7fe0432b947ac204107a9167ea59df
GarenLiang <ideabreeze@gmail.com>
2017-05-25 23:31:44 -0700

    main window class

---
+++ app/main_window.js
@@ -0,0 +1,21 @@
+const electron = require('electron');
+const { BrowserWindow } = electron;
+
+class MainWindow extends BrowserWindow {
+  constructor(url) {
+    super({
+      titleBarStyle: 'hidden',
+      height: 550,
+      width: 350,
+      show: false
+    });
+    this.loadURL(url);
+    this.on('blur', this.onBlur.bind(this));
+  }
+
+  onBlur() {
+    this.hide();
+  }
+}
+
+module.exports = MainWindow;
